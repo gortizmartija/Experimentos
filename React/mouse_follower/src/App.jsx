1ee91928e4cd674bd5d2ed13b22250384bf34801
gortizmartija <gortizmartija@gmail.com>
2025-09-26 14:03:59 +0200

    terminado seguimiento puntero

--- React/mouse_follower/src/App.jsx
+++ React/mouse_follower/src/App.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Proyecto 3</h1>;
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      if (!enabled) return;
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (!enabled) return;
+    window.addEventListener('pointermove', handleMove);
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+    };
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(78, 77, 30, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <h1>Proyecto Puntero</h1>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} el seguimiento del puntero
+      </button>
+    </>
+  );
 }
 
 export default App;
